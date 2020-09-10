db7bd0343836e4d8154a580dd304eaf88fbc895d
pgomesdev <pgomesdev@gmail.com>
2020-09-10 19:38:58 -0300

    feat(transaction): created list and create transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialBalance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    return this.transactions.reduce(
+      (acc: Balance, transaction: Transaction): Balance => {
+        const { outcome, income, total } = acc;
+
+        if (transaction.type === 'income') {
+          return {
+            outcome,
+            income: income + transaction.value,
+            total: total + transaction.value,
+          };
+        }
+
+        return {
+          outcome: outcome + transaction.value,
+          income,
+          total: total - transaction.value,
+        };
+      },
+      initialBalance,
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    const newTransaction = new Transaction({ title, type, value });
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error(
+        "Can't create new outcome transaction where the value is higher than the total balance",
+      );
+    }
+
+    return this.transactionsRepository.create({ title, type, value });
   }
 }
 
